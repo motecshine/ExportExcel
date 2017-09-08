08d95596dca502ceca374990b484271a6e552dfa
motecshine <shinemotec@gmail.com>
2017-09-08 17:06:16 +0800

    wip

--- src/ExportExcel.php
+++ src/ExportExcel.php
@@ -7,26 +7,91 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class ExportExcel {
     private $config;
+    private $data;
     private $cacheDriver;
-    public function __construct($config)
+    private $spreadsSheet;
+    private $char = [
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'
+    ];
+    public function __construct($config, array $data)
     {
+        if (empty($config)) {
+            new \Exception('Config Can Not Empty.');
+        }
+        if (empty($data)) {
+            new \Exception('Sheet Data Can Not Empty.');
+        }
         $this->config = $config;
+        $this->data = $data;
+        $this->spreadsSheet = new Spreadsheet;
         $this->cacheDriver = (new CacheDriver())->setCacheDriver($this->config['cache_driver']);
-
         if (! empty($this->cacheDriver)) {
             Settings::setCache($this->cacheDriver);
         }
     }
 
+    public function downloadPath()
+    {
+        return $this->config['path'] ? $this->config['path'] . '/' : './';
+    }
+
     public function write()
     {
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setCellValue('A1', 'Hello World !');
-        $writer = new Xlsx($spreadsheet);
-        $writer->save('hello world.xlsx');
-        dd($writer);
-
+        // 创建Table Header
+        $this->createTableHead();
+        dd(1);
     }
+
+    private function createTableHead()
+    {
+       $this->createSheetData(1);
+    }
+
+    private function createSheetData($sheetNumber)
+    {
+        $number = 0;
+        foreach ($this->config['table_header'] as $value) {
+            $this->spreadsSheet->getActiveSheet()->setCellValue($this->numberToString($number, $sheetNumber), $value);
+            $number++;
+        }
+        $writer = new Xlsx($this->spreadsSheet);
+        $writer->save($this->buildDownloadFileName());
+    }
+
+    private function buildDownloadFileName($ext = '.xls')
+    {
+        return $this->downloadPath() . $this->config['name'] . $ext;
+    }
+
+    private function numberToString($number, $sheetNumber)
+    {
+        return $this->char[$number] . $sheetNumber;
+    }
+
 }
 
