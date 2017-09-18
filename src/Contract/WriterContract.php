c0e4b1cbe8037b3f263dcdf533ff4146a94b72a1
motecshine <shinemotec@gmail.com>
2017-09-18 10:53:27 +0800

    fix: file name

---
+++ src/Contract/WriterContract.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Irain\ExportExcel\Contract;
+
+interface WriterContract
+{
+
+    public function buildAndOutStream($path);
+
+    public function save($path);
+
+    public function buildTable();
+
+    public function buildTableHeader();
+}
